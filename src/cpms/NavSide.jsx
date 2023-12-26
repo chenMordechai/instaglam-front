c9e411ce3fe5564829073607211ab02ce640e420
Chen <chen100030@gmail.com>
2023-12-26 14:52:14 +0200

    nav-side

--- src/cpms/NavSide.jsx
+++ src/cpms/NavSide.jsx
@@ -3,7 +3,24 @@
 export function NavSide() {
     return (
         <section className="nav-side">
-            <h1>NavSide</h1>
+            <div>
+                <img src="" alt="" />
+                <div>Me</div>
+                <button>Logout</button>
+            </div>
+
+            <h3>Suggested for you</h3>
+
+            <div>
+                <img src="" alt="" />
+                <div>name</div>
+                <button>Follow</button>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <div>name</div>
+                <button>Follow</button>
+            </div>
         </section>
     )
 }
