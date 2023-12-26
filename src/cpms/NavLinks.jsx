a665806844eeae98e7e404b27db31ee3be8eb9f0
Chen <chen100030@gmail.com>
2023-12-26 12:00:23 +0200

    nav links icons

--- src/cpms/NavLinks.jsx
+++ src/cpms/NavLinks.jsx
@@ -1,9 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass,faCompass,faClapperboard , faFilm , faLocationArrow,faHeart , faSquarePlus ,faBars} from '@fortawesome/free-solid-svg-icons'
+import instagram from '../assets/icons/instagram.svg'
+import compass from '../assets/icons/compass-regular.svg'
+import house from '../assets/icons/house-solid.svg'
+import plus from '../assets/icons/square-plus-regular.svg'
+
 
 
 export function NavLinks() {
     return (
         <section className="nav-links">
-            <h1>NavLinks</h1>
+            <img src={instagram} />
+
+            <img src={house} />
+
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            
+            <img src={compass} />
+             
+            <FontAwesomeIcon icon={faFilm} />
+            
+            <FontAwesomeIcon icon={faLocationArrow} /> {/*download!!!! */}
+          
+            <FontAwesomeIcon icon={faHeart} />
+
+            <img src={plus} />
+
+           <button>img</button>
+           
+            <FontAwesomeIcon icon={faBars} />
         </section>
     )
 }
