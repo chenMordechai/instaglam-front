0e2d028db2372992c022247f3b50a4816b62124c
Chen <chen100030@gmail.com>
2023-12-26 12:51:59 +0200

    nav links design

--- src/cpms/NavLinks.jsx
+++ src/cpms/NavLinks.jsx
@@ -1,34 +1,46 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMagnifyingGlass,faCompass,faClapperboard , faFilm , faLocationArrow,faHeart , faSquarePlus ,faBars} from '@fortawesome/free-solid-svg-icons'
 import instagram from '../assets/icons/instagram.svg'
 import compass from '../assets/icons/compass-regular.svg'
-import house from '../assets/icons/house-solid.svg'
+import heart from '../assets/icons/heart-regular.svg'
 import plus from '../assets/icons/square-plus-regular.svg'
-
-
+import message from '../assets/icons/message-regular.svg'
+import house from '../assets/icons/house-solid.svg'
+import glass from '../assets/icons/glass-solid.svg'
+import film from '../assets/icons/film-solid.svg'
+import bars from '../assets/icons/bars-solid.svg'
 
 export function NavLinks() {
     return (
         <section className="nav-links">
+            <a title="Instaglam" href="">
             <img src={instagram} />
-
+            </a>
+            <a title="Home" href="">
             <img src={house} />
-
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-            
+            </a>
+            <a title="Search" href="">
+            <img src={glass} />
+            </a>
+            <a  title="Explore" href="">
             <img src={compass} />
-             
-            <FontAwesomeIcon icon={faFilm} />
-            
-            <FontAwesomeIcon icon={faLocationArrow} /> {/*download!!!! */}
-          
-            <FontAwesomeIcon icon={faHeart} />
-
+            </a>
+            <a title="Reels" href="">
+            <img src={film} />
+            </a>
+            <a title="Messages" href="">
+            <img src={message} />
+            </a>
+            <a title="Notifications" href="">
+            <img src={heart} />
+            </a>
+            <a title="New Post" href="">
             <img src={plus} />
-
-           <button>img</button>
-           
-            <FontAwesomeIcon icon={faBars} />
+            </a>
+            <a title="Profile" href="">
+            {/* <span>img</span> */}
+            </a>
+            <a title="More" href="">
+            <img src={bars} />
+            </a>               
         </section>
     )
 }
