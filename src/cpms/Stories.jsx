fa2428a0085c556d204556519101b0dc7008dbdd
Chen <chen100030@gmail.com>
2023-12-27 11:29:35 +0200

    stories carousel dragging

--- src/cpms/Stories.jsx
+++ src/cpms/Stories.jsx
@@ -1,28 +1,59 @@
 import { Story } from "./Story";
 import arrowRight from '../assets/icons/caret-right-solid.svg'
 import arrowLeft from '../assets/icons/caret-left-solid.svg'
+import { useEffect, useRef, useState } from "react";
+import { faL } from "@fortawesome/free-solid-svg-icons";
 
 
 export function Stories() {
-    const stories = [{name:'mosheddddddd'},{name:'david'},{name:'Puki.mkfe'},{name:'shuki.g.w3ef'},{name:'momo.lalal'},{name:'nono'},{name:'lolo'},{name:'dfdf'},{name:'mosheddddddd'},{name:'david'}]
-    console.log('stories:', stories)
+    const stories = [{ name: 'mosheddddddd' }, { name: 'david' }, { name: 'Puki.mkfe' }, { name: 'shuki.g.w3ef' }, { name: 'momo.lalal' }, { name: 'nono' }, { name: 'lolo' }, { name: 'dfdf' }, { name: 'mosheddddd' }, { name: 'daviddd' }]
+    // console.log('stories:', stories)
+    const [isDragging, setIsDragging] = useState()
+    const [startX, setStartX] = useState()
+    const [startScrollLeft, setStartScrollLeft] = useState()
+    let carousel = useRef()
+
+    useEffect(() => {
+        document.addEventListener('mouseup', dragStop)
+    }, [])
+
+    function dragStart(ev) {
+        setIsDragging(true)
+        setStartX(ev.pageX || ev.touches[0].clientX)
+        setStartScrollLeft(carousel.current.scrollLeft)
+    }
+    function dragging(ev) {
+        if (!isDragging) return
+        let x = ev.pageX || ev.touches[0].clientX
+        carousel.current.scrollLeft = startScrollLeft - (x - startX)
+    }
+
+    function dragStop() {
+        setIsDragging(false)
+    }
+
+    function moveCarousel(direction) {
+        const firstCardWidth = carousel.current.offsetWidth
+        carousel.current.scrollLeft += (direction === 'left') ? -firstCardWidth : firstCardWidth
+    }
+
     return (
         <section className="stories">
             <div className="wrapper">
-                <button  className="arrow">
-            <img src={arrowLeft} />
-
+                <button onClick={() => moveCarousel('left')} className="arrow">
+                    <img src={arrowLeft} />
                 </button>
-            <ul className="carousel">
-            {stories.map(story => <li className="card" key={story.name}>
-                <Story story={story}/>
-            </li> 
-            )}
-            </ul>
-            <button  className="arrow">
-            <img  src={arrowRight} />
-
-            </button  >
+                <ul ref={carousel} className={`carousel ${isDragging ? 'dragging' : ''}`}
+                    onMouseDown={dragStart} onMouseMove={dragging}
+                    onTouchStart={dragStart} onTouchMove={dragging}>
+                    {stories.map(story => <li className="card" key={story.name}>
+                        <Story story={story} isDragging={isDragging} />
+                    </li>
+                    )}
+                </ul>
+                <button onClick={() => moveCarousel('right')} className="arrow">
+                    <img src={arrowRight} />
+                </button  >
             </div>
         </section>
     )
--- src/cpms/Story.jsx
+++ src/cpms/Story.jsx
@@ -1,12 +1,12 @@
 
 
-export function Story({story}) {
+export function Story({ story, isDragging }) {
     return (
         <section className="story">
             <div className="gradient">
-            <img src="https://cdn.pixabay.com/photo/2020/10/11/19/51/cat-5646889_1280.jpg"/>
-                </div>
-          <span>{story.name}</span>
+                <img draggable="false" src="https://cdn.pixabay.com/photo/2020/10/11/19/51/cat-5646889_1280.jpg" />
+            </div>
+            <span>{story.name}</span>
         </section>
     )
 }
