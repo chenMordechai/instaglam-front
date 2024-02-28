20ca1f00d4a398e422383ae1af0dd39e3bcffe11
Chen <chen100030@gmail.com>
2024-02-28 15:14:03 +0200

    post option modal scss

--- src/cmps/PostOptionsModal.jsx
+++ src/cmps/PostOptionsModal.jsx
@@ -1,34 +1,74 @@
 import { Link } from "react-router-dom";
+import line from '../assets/icons/line.png'
+import bookmark from '../assets/icons/bookmark-regular.png'
+import rotate from '../assets/icons/rotate-solid.svg'
+import qrcode from '../assets/icons/qrcode-solid.svg'
+import clock from '../assets/icons/time.png'
+import heart from '../assets/icons/heart-slash.png'
+import comment from '../assets/icons/comment-slash.png'
+import pencil from '../assets/icons/pencil.png'
+import trash from '../assets/icons/trash.png'
 
 export function PostOptionsModal({ userId, isFollowing, onRemovePost, onToggleOptionsModal, isLoggedinUserPost, postId }) {
 
 
     return (
-        <section className="modal">
+        <section className="modal post-options-modal">
+            <div className="header">
+                <img onClick={onToggleOptionsModal} className="line" src={line} />
+                <div className="actions-container">
+                    <div className="action">
+                        <div className="img-container">
+                            <img src={bookmark} />
+                        </div>
+                        <span>Save</span>
+                    </div>
+                    {/* {isLoggedinUserPost && <div className="action">
+                        <div className="img-container">
+                            <img src={rotate} />
+                        </div>
+                        <span>Remix</span>
+                    </div>} */}
+                    <div className="action">
+                        <div className="img-container">
+                            <img src={qrcode} />
+                        </div>
+                        <span>QR code</span>
+                    </div>
+                </div>
+            </div>
+
+            {isLoggedinUserPost && <>
+
+                <button className="option-container">
+                    <img src={clock} />
+                    Archive
+                </button>
+                <button className="option-container">
+                    <img src={heart} />
+                    Hide like count
+                </button>
+                <button className="option-container">
+                    <img src={comment} />
+                    Turn off commenting
+                </button>
+
+                <Link className="option-container" to={`/post/edit/${postId}`}>
+                    <img src={pencil} />
+                    Edit
+                </Link>
+
+                <button onClick={() => onRemovePost()} className="clr-red option-container">
+                    <img src={trash} />
+                    Delete
+                </button>
+            </>}
+
             {!isLoggedinUserPost &&
                 <Link to={`/profile/${userId}/posts`} >
                     About this account
                 </Link>}
 
-            <button className="doesnt-work">Add to favorites</button>
-            <button className="doesnt-work">Go to post</button>
-            <button className="doesnt-work">Share to..</button>
-            <button className="doesnt-work">Copy Link</button>
-            <button className="doesnt-work">Save</button>
-
-            {isLoggedinUserPost && <>
-
-                <button className="doesnt-work">Archive</button>
-                <Link to={`/post/edit/${postId}`}>
-                    Edit
-                </Link>
-
-                <button onClick={() => onRemovePost()} className="clr-red">
-                    Delete
-                </button>
-            </>}
-
-            <button onClick={onToggleOptionsModal} >Cancel</button>
 
         </section>
     )
