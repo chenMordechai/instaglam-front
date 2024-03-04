893b7953f4229af5012c1bf02089da2ab063170b
Chen <chen100030@gmail.com>
2024-03-04 22:08:21 +0200

    added service-worker

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.jsx'
 // import './index.css'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-import swDev from './swDev.js'
+// import swDev from './swDev.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <App />
   // </React.StrictMode>,
-  )
-  
-swDev()
+)
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// swDev()
